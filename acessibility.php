1a4c55ca828549db2c454b334a6afac70cab8bdc
2011Alexandrealvarenga <2011brunofernandes@gmail.com>
2024-09-18 11:50:32 -0300

    update

---
+++ acessibility.php
@@ -0,0 +1,14 @@
+<?php
+/*
+ * Plugin Name: Acessibility Plugin
+ * Description: Plugin that have somes function to acessibilily
+ * Version: 2.0
+ * Author: Alexandre Alvarenga
+ * Plugin URI: 
+ * Author URI: 
+ */
+
+ if(!function_exists('add_action')){
+  echo 'Hi, I am a plugin, i cannot call directly!';
+  exit;
+}
